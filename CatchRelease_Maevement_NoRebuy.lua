1059e6ec85ed9b4a1ec8986515dab3d26f5838b4
MaeBot <69312348+MaeBot@users.noreply.github.com>
2020-08-26 17:23:18 +0200

    Add files via upload

---
+++ CatchRelease_Maevement_NoRebuy.lua
@@ -0,0 +1,264 @@
+import("TemBot.Lua.TemBotLua")
+
+--Registering the Temtem Window
+tblua:RegisterTemTemWindow()
+
+-- define turn
+t = 1
+
+tblua:Sleep(1500)
+
+MovementSwitch = 1
+
+if tblua:IsInWorld() == true then
+ while(true)
+ do
+   if tblua:IsInWorld() == true then
+      local sexy = math.random(50, 250)
+      tblua:Sleep(sexy)
+      tblua:CheckPause()
+      tblua:CheckLogout()
+    while tblua:IsInWorld() == true do
+       t = 1
+       while MovementSwitch == 1 do
+         if tblua:IsInWorld() == false then
+           break
+         else
+           tblua:CircleArea()
+         end
+       end
+       while MovementSwitch == 2 do
+         if tblua:IsInWorld() == false then
+           break
+         else
+           tblua:RandomArea()
+         end
+       end
+    end
+   end
+
+            --loop if minimap not detected
+            while tblua:IsInWorld() == false do
+      local sex = math.random(50, 150)
+      tblua:Sleep(sex)
+      tblua:StopMovement()
+                --if bot is in fight
+                if tblua:IsInFight() == true then
+                    --Luma check positive (message and notification)
+        local Platypet = math.random(100, 200)
+        tblua:Sleep(Platypet)
+                    if tblua:CheckLuma() == true then
+                        tblua:SendTelegramMessage("Luma Found! Waiting...")
+                        tblua:TestMessage("Luma Found! Waiting...")
+                        tblua:PressKey(0x71)
+                    else
+                    local RandomSleepy = tblua:GetSleepTime()
+                    local cr = tblua:GetSleepTime()
+                    -- exhausted
+                    if tblua:GetPixelColor(276, 631) == "0xFFFFFF" then
+                    tblua:Sleep(RandomSleepy)
+                    tblua:PressKey(0x36)
+                    tblua:Sleep(RandomSleepy)
+                    else
+                      if tblua:GetPixelColor(777, 65) == "0x1E1E1E" then
+                      if tblua:GetPixelColor(1045, 100) == "0x1E1E1E" then
+                        if t > 3 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        elseif t == 3 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x45)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x26)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        elseif t == 2 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x31)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x26)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x31)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        elseif t == 1 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x31)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x31)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        end
+                       else
+                         if t > 2 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        elseif t == 2 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x45)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        elseif t == 1 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x31)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x31)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        end
+                       end
+                      elseif tblua:GetPixelColor(1045, 100) == "0x1E1E1E" then
+                         if t > 2 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        elseif t == 2 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x45)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x37)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        elseif t == 1 then
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x31)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x31)
+                         tblua:Sleep(cr)
+                         tblua:PressKey(0x46)
+                         t = t + 1
+                        end
+                      end
+                     end
+                   end        
+                else
+                    local crx = tblua:GetSleepTime()
+                    local crxxx = math.random(1534, 2857)
+                    -- Catch screen Release
+                    if tblua:GetPixelColor(750, 530) == "0x1CD1D3" then
+                    tblua:Sleep(crxxx)
+                    tblua:PressKey(0x27)
+                    tblua:Sleep(crx)
+                    tblua:PressKey(0x46)
+                    tblua:Sleep(crx)
+                    tblua:PressKey(0x46)
+                    tblua:Sleep(crx)
+                    -- Skill learn
+                    elseif tblua:GetPixelColor(215, 255) == "0x1CD1D3" then
+                        tblua:PressKey(0x1B)
+                    tblua:Sleep(crx)
+                    -- Exp screen F
+                    elseif tblua:GetPixelColor(590, 245) == "0x1CD1D3" then
+                    tblua:PressKey(0x1B)
+                    tblua:Sleep(crx)
+                    -- Swap dead tem
+                    elseif tblua:GetPixelColor(1180, 455) == "0x1E1E1E" then
+                    local swap = math.random(4)
+                      if swap == 4 then
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x46)
+                        tblua:Sleep(crx)
+                      elseif swap == 3 then
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x46)
+                        tblua:Sleep(crx)
+                      elseif swap == 2 then
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x46)
+                       tblua:Sleep(crx)
+                      elseif swap == 1 then
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x28)
+                        tblua:Sleep(crx)
+                        tblua:PressKey(0x46)
+                        tblua:Sleep(crx)
+                      end
+                    else
+                    tblua:Sleep(crx)
+                    end
+                end
+            end
+    end
+else
+    tblua:TestMessage("Error: Must see minimap to start the script")
+end
